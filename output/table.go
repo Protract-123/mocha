d0d247539a84eaa6b1350546f36fe4f926ca1a8c
Yuvraj Verma <vermayuvraj2010@gmail.com>
2026-06-29 10:25:15 +0530

    fix(output): wrap errors in table.go w/context

--- output/table.go
+++ output/table.go
@@ -12,21 +12,21 @@ func PrintTable(headers []string, rows [][]string) error {
 
 	headerLine := strings.Join(headers, "\t")
 	if _, err := fmt.Fprintln(tableWriter, headerLine); err != nil {
-		return err
+		return fmt.Errorf("failed to write header line: %w", err)
 	}
 
 	separators := make([]string, len(headers))
-	for i, h := range headers {
-		separators[i] = strings.Repeat("-", len(h))
+	for i, header := range headers {
+		separators[i] = strings.Repeat("-", len(header))
 	}
 	if _, err := fmt.Fprintln(tableWriter, strings.Join(separators, "\t")); err != nil {
-		return err
+		return fmt.Errorf("failed to write separators: %w", err)
 	}
 
 	for _, row := range rows {
 		line := strings.Join(row, "\t")
 		if _, err := fmt.Fprintln(tableWriter, line); err != nil {
-			return err
+			return fmt.Errorf("failed to write a row: %w", err)
 		}
 	}
 
